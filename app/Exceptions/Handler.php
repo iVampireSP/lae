a2e65e688ab1cd764acb8f2b79aef19400cae48c
iVampireSP.com <im@ivampiresp.com>
2022-09-09 01:32:42 +0800

    改进 错误返回

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -49,6 +49,17 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        return parent::render($request, $exception);
+
+        $rendered = parent::render($request, $exception);
+
+        return response()->json(
+            [
+                'error' => [
+                    'code' => $rendered->getStatusCode(),
+                    'message' => $exception->getMessage(),
+                ]
+            ],
+            $rendered->getStatusCode()
+        );
     }
 }
