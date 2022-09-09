1067756f60e0fed6234ef37b49bb2c25906ea2a2
iVampireSP.com <im@ivampiresp.com>
2022-09-09 11:56:20 +0800

    改进 Redis 配置

--- config/database.php
+++ config/database.php
@@ -97,7 +97,8 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'lumen'), '_').'_database_'),
+            'scheme' => 'tls',
+            'prefix' => 'laecloud_database_',
         ],
 
         'default' => [
