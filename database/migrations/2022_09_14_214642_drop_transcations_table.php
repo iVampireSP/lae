6c81c37b7336892bf5be9824fb9d216bde2f728a
iVampireSP.com <im@ivampiresp.com>
2022-09-15 12:23:47 +0800

    fix

--- database/migrations/2022_09_14_214642_drop_transcations_table.php
+++ database/migrations/2022_09_14_214642_drop_transcations_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         //
-        Schema::dropDatabaseIfExists('transcations');
+        Schema::dropIfExists('transcations');
     }
 
     // /**
