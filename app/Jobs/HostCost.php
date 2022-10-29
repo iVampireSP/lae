4b57077d9fbc027f05baba9bed8594f01654a353
iVampireSP.com <im@ivampiresp.com>
2022-10-29 11:24:37 +0800

    删除 多余的代码

--- app/Jobs/HostCost.php
+++ app/Jobs/HostCost.php
@@ -35,39 +35,9 @@ class HostCost implements ShouldQueue
         // $this->cache = new Cache();
 
         // chunk hosts and load user
-        Host::active()->with('user')->chunk(100, function ($hosts) {
+        Host::active()->with('user')->chunk(1000, function ($hosts) {
             foreach ($hosts as $host) {
-
                 $host->cost();
-
-                //     $this->cache_key = 'user_' . $host->user_id;
-
-                //     // if cache has user
-
-                //     if ($this->cache->has($this->cache_key)) {
-                //         // if user is not instances of Model
-                //         $user = $this->cache->get($this->cache_key);
-
-                //         if ($user instanceof User) {
-                //             $this->user = $user;
-                //         } else {
-                //             $this->user = $this->cache->put($this->cache_key, $host->user, now()->addDay());
-                //         }
-                //     } else {
-                //         $this->user = $this->cache->put($this->cache_key, $host->user, now()->addDay());
-                //     }
-
-                //     // Log::debug($user);
-
-                //     if ($host->managed_price) {
-                //         $host->price = $host->managed_price;
-                //     }
-
-
-                //     $this->user->drops -= $host->price;
-
-                //     // update cache
-                //     $this->cache->put($this->cache_key, $this->user, now()->addDay());
             }
         });
     }
