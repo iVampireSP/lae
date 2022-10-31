70fcf76d7e6c60ed192b6595206a3af81b34b412
iVampireSP.com <im@ivampiresp.com>
2022-10-31 19:03:44 +0800

    默认配置

--- config/settings.php
+++ config/settings.php
@@ -3,9 +3,9 @@
 return [
     'wecom' => [
         'robot_hook' => [
-            'default' => '4727b740-57dc-448c-a89a-840e28fb48dc',
-            'billing' => '50aa88b6-febe-4cf5-a18f-c714b3752548',
-            'cluster_ready' => '7de76311-280e-40eb-bfd0-c17c0e053e90',
+            'default' => env('WECOM_ROBOT_HOOK_DEFAULT', ''),
+            'billing' => env('WECOM_ROBOT_HOOK_BILLING', ''),
+            'cluster_ready' => env('WECOM_ROBOT_HOOK_CLUSTER_READY', ''),
         ],
     ]
 ];
