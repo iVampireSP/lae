95a189a07c23a127054319dc69caf5a233108612
iVampireSP.com <im@ivampiresp.com>
2022-11-21 13:18:38 +0800

    改进 broadcast

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -15,8 +15,7 @@ class BroadcastServiceProvider extends ServiceProvider
     public function boot()
     {
         Broadcast::routes([
-            'middleware' => ['auth:sanctum'],
-            'prefix' => 'api',
+            'middleware' => ['auth:sanctum', 'auth'],
         ]);
 
         require base_path('routes/channels.php');
