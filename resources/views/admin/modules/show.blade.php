bc27fa367ca0ae22c01789118a9d2bbc753c1a5c
iVampireSP.com <im@ivampiresp.com>
2022-11-24 08:47:04 +0800

    Update show.blade.php

--- resources/views/admin/modules/show.blade.php
+++ resources/views/admin/modules/show.blade.php
@@ -19,8 +19,9 @@
             <th>名称</th>
             <th>用户</th>
             <th>月估算价格</th>
-            <th>创建时间</th>
+            <th>状态</th>
             <th>更新时间</th>
+            <th>创建时间</th>
             <th>操作</th>
         </thead>
 
@@ -42,10 +43,13 @@
                     {{ $host->price }} 元
                 </td>
                 <td>
-                    {{ $host->created_at }}
+                    <x-host-status :status="$host->status" />
                 </td>
                 <td>
                     {{ $host->updated_at }}
+                </td>
+                <td>
+                    {{ $host->created_at }}
                 </td>
                 <td>
                     <a href="{{ route('admin.hosts.edit', $host) }}" class="btn btn-primary btn-sm">编辑</a>
