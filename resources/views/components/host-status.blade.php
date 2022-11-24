2a298a1cdcca99507124e4247df93a55c451aa6b
iVampireSP.com <im@ivampiresp.com>
2022-11-24 08:41:36 +0800

    Add files via upload

---
+++ resources/views/components/host-status.blade.php
@@ -0,0 +1,22 @@
+<span>
+    @switch($status)
+        @case('running')
+            <span class="badge bg-success">运行中</span>
+        @break
+
+        @case('suspended')
+            <span class="badge bg-warning">已暂停</span>
+        @break
+
+        @case('stopped')
+            <span class="badge bg-danger">已停止</span>
+        @break
+
+        @case('pending')
+            <span class="badge bg-info">创建中</span>
+        @break
+
+        @default
+            <span class="badge bg-secondary">{{ $status_str }}</span>
+    @endswitch
+</span>
