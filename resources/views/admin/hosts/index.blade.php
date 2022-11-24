b9a861a9d25b5e903d580acc1779a7eda9b0bbcc
iVampireSP.com <im@ivampiresp.com>
2022-11-24 08:45:36 +0800

    Update index.blade.php

--- resources/views/admin/hosts/index.blade.php
+++ resources/views/admin/hosts/index.blade.php
@@ -12,8 +12,9 @@
             <th>名称</th>
             <th>用户</th>
             <th>月估算价格</th>
-            <th>创建时间</th>
+            <th>状态</th>
             <th>更新时间</th>
+            <th>创建时间</th>
             <th>操作</th>
             </thead>
 
@@ -43,16 +44,18 @@
                         @endif
                     </td>
                     <td>
-                        {{ $host->created_at }}
+                        <x-host-status :status="$host->status" />
                     </td>
                     <td>
                         {{ $host->updated_at }}
                     </td>
                     <td>
+                        {{ $host->created_at }}
+                    </td>
+                    <td>
                         <a href="{{ route('admin.hosts.edit', $host) }}" class="btn btn-primary btn-sm">编辑</a>
                     </td>
                 </tr>
-
             @endforeach
             </tbody>
         </table>
