d789e6fa56eb4585ff14b3be328bac633635e6b7
iVampireSP.com <im@ivampiresp.com>
2022-11-25 15:49:55 +0800

    增加 错误

--- resources/views/components/host-status.blade.php
+++ resources/views/components/host-status.blade.php
@@ -2,19 +2,23 @@
     @switch($status)
         @case('running')
             <span class="badge bg-success">运行中</span>
-        @break
+            @break
 
         @case('suspended')
             <span class="badge bg-warning">已暂停</span>
-        @break
+            @break
 
         @case('stopped')
             <span class="badge bg-danger">已停止</span>
-        @break
+            @break
 
         @case('pending')
             <span class="badge bg-info">创建中</span>
-        @break
+            @break
+
+        @case('error')
+            <span class="badge bg-danger">错误</span>
+            @break
 
         @default
             <span class="badge bg-secondary">{{ $status }}</span>
