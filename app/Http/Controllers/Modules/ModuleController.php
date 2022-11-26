8a9044bb60fb1634bd94683a61ead233484b2eaa
iVampireSP.com <im@ivampiresp.com>
2022-11-26 20:20:47 +0800

    改进 响应

--- app/Http/Controllers/Modules/ModuleController.php
+++ app/Http/Controllers/Modules/ModuleController.php
@@ -7,6 +7,7 @@ use App\Models\Module;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 
 class ModuleController extends Controller
@@ -22,15 +23,6 @@ class ModuleController extends Controller
         $path = $this->fixPath($request, $module, 'api');
 
         $method = Str::lower($request->method());
-
-        // 如果 method 为 post, 检查用户余额
-        if ($method == 'post') {
-            $user = auth()->user();
-
-            if ($user->balance < 1) {
-                return $this->error('账户余额不足，请保证账户余额至少有 1 元。');
-            }
-        }
 
         $response = $module->request($method, $path, $request->all());
 
