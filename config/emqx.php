11cbcd4280393f7e4acd8d9bd89e0c483be53683
iVampireSP.com <im@ivampiresp.com>
2022-11-28 23:24:53 +0800

    改名

--- config/emqx.php
+++ config/emqx.php
@@ -3,5 +3,5 @@
 return [
     'api_url' => env('EMQX_API_URL'),
     'api_key' => env('EMQX_API_KEY'),
-    'api_secret' => env('EMQX_API_SECRET'),
+    'api_secret' => env('EMQX_API_SECRET_KEY'),
 ];
