0db209ba7290a12152f3be569e664591c8a06b90
iVampireSP.com <im@ivampiresp.com>
2022-12-28 21:18:38 +0800

    增加 payment

--- resources/views/notifications/user/balance.blade.php
+++ resources/views/notifications/user/balance.blade.php
@@ -1,3 +1,3 @@
 @if ($balance->paid_at !== null)
-    {{ $user->name }} 在 {{ $balance->paid_at->toDateTimeString() }} 充值了 {{ $balance->amount }} 元。
+    {{ $user->name }} 在 {{ $balance->paid_at->toDateTimeString() }} 通过 {{ $balance->payment }} 充值了 {{ $balance->amount }} 元。
 @endif
