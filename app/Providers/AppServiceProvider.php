68f28a32c6e776fd513564b1ada2aa6600657288
iVampireSP.com <im@ivampiresp.com>
2023-01-02 19:00:38 +0800

    改进 返回类型

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
+    public function register(): void
     {
         //
     }
