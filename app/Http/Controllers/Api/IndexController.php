1501089869641e59885fff06051289fcf8b898c4
iVampireSP.com <im@ivampiresp.com>
2023-01-02 18:53:50 +0800

    增加 API 根目录提供客户端 IP 地址

--- app/Http/Controllers/Api/IndexController.php
+++ app/Http/Controllers/Api/IndexController.php
@@ -5,20 +5,20 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    //
-    public function index(): JsonResponse
+    public function index(Request $request): JsonResponse
     {
         return $this->success([
             'message' => 'Welcome to LaeCloud API Server.',
+            'ip' => $request->ip(),
         ]);
     }
 
     public function birthdays(): JsonResponse
     {
-        // 获取今天过生日的用户，每页显示 20 个,使用 carbon
         $users = User::birthday()->simplePaginate(20);
 
         return $this->success($users);
