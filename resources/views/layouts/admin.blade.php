7ab9e0b6988d084875f3b8939618449f035a9b7d
iVampireSP.com <im@ivampiresp.com>
2023-01-05 21:09:29 +0800

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -28,7 +28,7 @@
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
+                <span class="bi bi-list text-auto fs-1"></span>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
