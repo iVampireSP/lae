5be9c721dffb82c3920294c2a298fbb11c7ad562
iVampireSP.com <im@ivampiresp.com>
2023-01-10 20:44:32 +0800

    增加 广播鉴权

--- routes/channels.php
+++ routes/channels.php
@@ -6,6 +6,16 @@ Broadcast::channel('users.{userId}', function ($user, $userId) {
     return (int)$user->id === (int)$userId;
 });
 
+Broadcast::channel('tasks.{userId}', function ($user, $userId) {
+    return (int)$user->id === (int)$userId;
+});
+
+
+Broadcast::channel('work-orders.{uuid}', function () {
+    return true;
+});
+
+
 Broadcast::channel('servers', function () {
     return true;
 });
