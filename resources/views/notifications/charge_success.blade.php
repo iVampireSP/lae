7640b889f94a269d517bc526f27756ba333de0ec
iVampireSP.com <im@ivampiresp.com>
2023-01-13 22:02:30 +0800

    改进 用户充值提醒

--- resources/views/notifications/charge_success.blade.php
+++ resources/views/notifications/charge_success.blade.php
@@ -1,3 +1,3 @@
 @if ($data['balance']->paid_at !== null)
-    {{ $data['user']->name }} 在 {{ $data['balance']->paid_at->toDateTimeString() }} 通过 <x-payment :payment="$data['balance']->payment" /> 充值了 {{ $data['balance']->amount }} 元。
+    {{ $data['user']->name }} 在 {{ $data['balance']->paid_at->toDateTimeString() }} 充值了 {{ $data['balance']->amount }} 元，使用了 <x-payment :payment="$data['balance']->payment" />
 @endif
