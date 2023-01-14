f937b2541b5372b5e981143ec05894057c2357a4
iVampireSP.com <im@ivampiresp.com>
2023-01-15 07:51:11 +0800

    增加 warning

--- resources/views/components/alert-warning.blade.php
+++ resources/views/components/alert-warning.blade.php
@@ -1,3 +1,10 @@
 <div>
-    <!-- Because you are alive, everything is possible. - Thich Nhat Hanh -->
+    <div class="alert alert-warning d-flex align-items-center fade show" role="alert">
+        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Warning:">
+            <use xlink:href="#exclamation-triangle-fill"/>
+        </svg>
+        <div>
+            {{ $slot }}
+        </div>
+    </div>
 </div>
