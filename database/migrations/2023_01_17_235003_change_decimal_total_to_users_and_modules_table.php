c549a4dcaeadbff79be40f8881cebd3c1488481f
iVampireSP.com <im@ivampiresp.com>
2023-01-18 00:06:49 +0800

    改进 decimal total

---
+++ database/migrations/2023_01_17_235003_change_decimal_total_to_users_and_modules_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->decimal('balance', 20, 4)->change();
+        });
+
+        Schema::table('modules', function (Blueprint $table) {
+            $table->decimal('balance', 20, 4)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->decimal('balance', 10)->change();
+        });
+
+        Schema::table('modules', function (Blueprint $table) {
+            $table->decimal('balance', 10)->change();
+        });
+    }
+};
