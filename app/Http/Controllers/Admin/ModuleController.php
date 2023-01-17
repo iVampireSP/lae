80d64b2f84a2f4b219b11bc8794d5540e0798f71
iVampireSP.com <im@ivampiresp.com>
2023-01-18 01:29:36 +0800

    移除 多余的导入

--- app/Http/Controllers/Admin/ModuleController.php
+++ app/Http/Controllers/Admin/ModuleController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\Host;
 use App\Models\Module;
 use App\Models\ModuleAllow;
-use App\Models\Transaction;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
