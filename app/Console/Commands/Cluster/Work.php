656648cc83dd32ade0a15e9cf8dab30d5286f7ef
iVampireSP.com <im@ivampiresp.com>
2023-01-18 19:20:22 +0800

    改进 不再刷写 node_id

--- app/Console/Commands/Cluster/Work.php
+++ app/Console/Commands/Cluster/Work.php
@@ -71,33 +71,15 @@ class Work extends Command
         // 关闭 Octane
         Artisan::call('octane:stop');
 
-        Artisan::call('config:cache');
-
         if (!config('settings.node.ip')) {
             $this->error('请先配置节点 IP。');
             return CommandAlias::FAILURE;
         }
 
-
-        // 重写 .env 文件中的 NODE_ID
-        $this->info('正在重写 .env 文件中的 NODE_ID。');
-
-        $node_id = Str::random(8);
-
-        if (config('settings.node.type') === 'master') {
-            $node_id = 'master';
-        }
-
-        $env = file_get_contents(base_path('.env'));
-
-        $env = preg_replace('/^NODE_ID=.*$/m', 'NODE_ID=' . $node_id, $env);
-
-        file_put_contents(base_path('.env'), $env);
-
-
         // 刷新配置缓存
         $this->info('正在刷新配置缓存。');
         Artisan::call('config:cache');
+        Artisan::call('route:cache');
 
         // redis 创建一个 hash
         $this->info('正在注册节点。');
