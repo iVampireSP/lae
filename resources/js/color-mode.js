408bfdd018c32e52419954f7bd6eb020cd96add4
iVampireSP.com <im@ivampiresp.com>
2023-01-18 17:44:05 +0800

    改进 颜色切换

--- resources/js/color-mode.js
+++ resources/js/color-mode.js
@@ -27,28 +27,6 @@
 
     setTheme(getPreferredTheme())
 
-    const showActiveTheme = (theme, focus = false) => {
-        const themeSwitcher = document.querySelector('#bd-theme')
-        const themeSwitcherText = document.querySelector('#bd-theme-text')
-        const activeThemeIcon = document.querySelector('.theme-icon-active use')
-        const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
-        const svgOfActiveBtn = btnToActive.querySelector('svg use').getAttribute('href')
-
-        document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
-            element.classList.remove('active')
-            element.setAttribute('aria-pressed', 'false')
-        })
-
-        btnToActive.classList.add('active')
-        btnToActive.setAttribute('aria-pressed', 'true')
-        activeThemeIcon.setAttribute('href', svgOfActiveBtn)
-        const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`
-        themeSwitcher.setAttribute('aria-label', themeSwitcherLabel)
-
-        if (focus) {
-            themeSwitcher.focus()
-        }
-    }
 
     window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
         if (storedTheme !== 'light' || storedTheme !== 'dark') {
@@ -57,15 +35,12 @@
     })
 
     window.addEventListener('DOMContentLoaded', () => {
-        showActiveTheme(getPreferredTheme())
-
         document.querySelectorAll('[data-bs-theme-value]')
             .forEach(toggle => {
                 toggle.addEventListener('click', () => {
                     const theme = toggle.getAttribute('data-bs-theme-value')
                     localStorage.setItem('theme', theme)
                     setTheme(theme)
-                    showActiveTheme(theme, true)
                 })
             })
     })
