d0edde1a53825b289df7b09c762b1aecee09acf3
iVampireSP.com <im@ivampiresp.com>
2023-02-02 20:43:18 +0800

    改进 登录页面

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,91 +1,53 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Login') }}</div>
 
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
+    <h2>欢迎使用 {{ config('app.display_name') }}</h2>
 
-                            <div class="row mb-3">
-                                <label for="email"
-                                       class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+    <form action="{{ route('login') }}" method="POST">
+        @csrf
 
-                                <div class="col-md-6">
-                                    <input id="email" type="email"
-                                           class="form-control @error('email') is-invalid @enderror" name="email"
-                                           value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                    @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="row mb-3">
-                                <label for="password"
-                                       class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="password" type="password"
-                                           class="form-control @error('password') is-invalid @enderror" name="password"
-                                           required autocomplete="current-password">
-
-                                    @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="row mb-3">
-                                <div class="col-md-6 offset-md-4">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" name="remember"
-                                               id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                        <label class="form-check-label" for="remember">
-                                            {{ __('Remember Me') }}
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="row mb-0">
-                                <div class="col-md-8 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Login') }}
-                                    </button>
-
-                                    <a class="btn btn-link" href="{{ route('register') }}">
-                                        {{ __('Register') }}
-                                    </a>
-
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-
-                                </div>
-
-                                <div class="text-center mt-3">如果您继续，则代表您已经阅读并同意 <a
-                                        href="https://www.laecloud.com/tos/"
-                                        target="_blank"
-                                        class="text-decoration-underline">服务条款</a>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-
+        <div class="form-group">
+            <label for="email" class="text-left ml-0">邮箱</label>
+            <input type="email" name="email" id="email" class="form-control mb-3" required autofocus>
         </div>
-    </div>
 
+        <div class="form-group">
+            <label for="password">密码</label>
+            <input type="password" id="password" name="password"
+                   class="form-control rounded-right" required>
+        </div>
+
+        <div class="form-group mt-2">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="remember" checked>
+                <label class="form-check-label" for="remember">
+                    记住登录
+                </label>
+            </div>
+        </div>
+
+        <div class="mt-1">如果您继续，则代表您已经阅读并同意 <a
+                href="https://www.laecloud.com/tos/"
+                target="_blank"
+                class="text-decoration-underline">服务条款</a>
+        </div>
+
+
+        <button class="btn btn-primary btn-block mt-2" type="submit">
+            登录
+        </button>
+
+
+    </form>
+
+    <br/>
+
+    <a class="link" href="{{ route('register') }}">
+        {{ __('Register') }}
+    </a>
+    &nbsp;
+    <a class="link" href="{{ route('password.request') }}">
+        {{ __('Forgot Your Password?') }}
+    </a>
 @endsection
