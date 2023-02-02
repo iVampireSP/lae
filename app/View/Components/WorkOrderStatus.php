ea75f9b43cc8748064de8eeb9c2cc25624fb0319
iVampireSP.com <im@ivampiresp.com>
2023-02-02 20:12:01 +0800

    改进 工单

--- app/View/Components/WorkOrderStatus.php
+++ app/View/Components/WorkOrderStatus.php
@@ -26,19 +26,6 @@ class WorkOrderStatus extends Component
      */
     public function render(): View
     {
-        $this->status = match ($this->status) {
-            'pending' => '推送中',
-            'open' => '开启',
-            'user_read' => '用户已读',
-            'user_replied' => '用户已回复',
-            'replied' => '已回复',
-            'read' => '已读',
-            'on_hold' => '挂起',
-            'in_progress' => '处理中',
-            'closed' => '结单',
-            default => $this->status,
-        };
-
         return view('components.work-order-status', ['status' => $this->status]);
     }
 }
--- resources/views/components/work-order-status.blade.php
+++ resources/views/components/work-order-status.blade.php
@@ -1,1 +1,46 @@
-<span>{{ $status }}</span>
+<span>
+    @switch($status)
+        @case('pending')
+            <span class="badge bg-info">推送中</span>
+            @break
+
+        @case('open')
+            <span class="badge bg-success">开启</span>
+            @break
+
+        @case('user_read')
+            <span class="badge bg-success">用户已读</span>
+            @break
+
+        @case('user_replied')
+            <span class="badge bg-warning">用户已回复</span>
+            @break
+
+        @case('replied')
+            <span class="badge bg-success">已回复</span>
+            @break
+
+        @case('read')
+            <span class="badge bg-success">已读</span>
+            @break
+
+        @case('on_hold')
+            <span class="badge bg-secondary">挂起</span>
+            @break
+
+        @case('in_progress')
+            <span class="badge bg-secondary">处理中</span>
+            @break
+
+        @case('closed')
+            <span class="badge bg-secondary">关闭</span>
+            @break
+
+        @case('error')
+            <span class="badge bg-danger">错误</span>
+            @break
+
+        @default
+            <span class="badge bg-secondary">{{ $status }}</span>
+    @endswitch
+</span>
