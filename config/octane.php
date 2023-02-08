5762c8a3d6fa32d45bfb06bd169ab9897ad33b11
iVampireSP.com <im@ivampiresp.com>
2023-02-08 20:49:16 +0800

    改进

--- config/octane.php
+++ config/octane.php
@@ -42,7 +42,7 @@ return [
     |
     | When this configuration value is set to "true", Octane will inform the
     | framework that all absolute links must be generated using the HTTPS
-    | protocol. Otherwise your links may be generated using plain HTTP.
+    | protocol. Otherwise, your links may be generated using plain HTTP.
     |
     */
 
