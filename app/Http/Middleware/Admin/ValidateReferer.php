717e681fcf26d9aba7e3fb2034c4aa673415a9b6
iVampireSP.com <im@ivampiresp.com>
2023-02-10 02:05:49 +0800

    改进 返回为 mixed

--- app/Http/Middleware/Admin/ValidateReferer.php
+++ app/Http/Middleware/Admin/ValidateReferer.php
@@ -15,9 +15,9 @@ class ValidateReferer
      *
      * @param  Request  $request
      * @param Closure(Request): (Response|RedirectResponse) $next
-     * @return Response|RedirectResponse
+     * @return mixed
      */
-    public function handle(Request $request, Closure $next): Response|RedirectResponse
+    public function handle(Request $request, Closure $next): mixed
     {
         // 如果 referer 不为空，且不是来自本站的请求，则返回 403
         if ($request->headers->get('referer') && ! Str::contains($request->headers->get('referer'), config('app.url'))) {
