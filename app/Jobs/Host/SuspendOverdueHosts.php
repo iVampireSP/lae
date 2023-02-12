8c28e45c9891d5e149a4cab51d1b10b28100eaf7
iVampireSP.com <im@ivampiresp.com>
2023-02-13 02:46:45 +0800

    增加 队列暂停

---
+++ app/Jobs/Host/SuspendOverdueHosts.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Jobs\Host;
+
+use App\Models\Host;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class SuspendOverdueHosts implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected ?Host $host;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(?Host $host = null)
+    {
+        $this->host = $host;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle(): void
+    {
+        if (!$this->host) {
+            Host::where('next_due_at', '<', now())
+                ->where('status', '!=', 'suspended')
+                ->chunk(100, function ($hosts) {
+                    foreach ($hosts as $host) {
+                        dispatch(new self($host));
+                    }
+                });
+        }
+
+        $this->host?->suspend();
+    }
+}
