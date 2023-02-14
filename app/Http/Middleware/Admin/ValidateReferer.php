bda41c34e513c0be1f23b1741f5823112bce71ec
iVampireSP.com <im@ivampiresp.com>
2023-02-14 20:11:32 +0800

    改进 测试环境不生效

--- app/Http/Middleware/Admin/ValidateReferer.php
+++ app/Http/Middleware/Admin/ValidateReferer.php
@@ -13,16 +13,19 @@ class ValidateReferer
     /**
      * Handle an incoming request.
      *
-     * @param  Request  $request
+     * @param Request                                       $request
      * @param Closure(Request): (Response|RedirectResponse) $next
+     *
      * @return mixed
      */
     public function handle(Request $request, Closure $next): mixed
     {
-        // return $next($request);
+        if (app()->environment('local')) {
+            return $next($request);
+        }
 
         // 如果 referer 不为空，且不是来自本站的请求，则返回 403
-        if ($request->headers->get('referer') && ! Str::contains($request->headers->get('referer'), config('app.url'))) {
+        if ($request->headers->get('referer') && !Str::contains($request->headers->get('referer'), config('app.url'))) {
             abort(403, '来源不属于后台。');
         } else {
             return $next($request);
