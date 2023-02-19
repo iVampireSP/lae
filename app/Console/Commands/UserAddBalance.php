d8503a04a3644565098807ef2fb8be9704943d39
iVampireSP.com <im@ivampiresp.com>
2023-02-20 00:10:09 +0800

    格式化代码

--- app/Console/Commands/UserAddBalance.php
+++ app/Console/Commands/UserAddBalance.php
@@ -2,7 +2,6 @@
 
 namespace App\Console\Commands;
 
-use App\Models\Transaction;
 use App\Models\User;
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Command\Command as CommandAlias;
