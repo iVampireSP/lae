15a32a8e7df5909017929ca10d972d29a8156a2d
iVampireSP.com <im@ivampiresp.com>
2023-02-20 00:59:36 +0800

    改进

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -12,7 +12,9 @@
             @csrf
             <button type="submit"
                     class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>
-            .
+            。
         </form>
+
+        <p>我们将会删除超过 3 天没有验证邮箱的用户。</p>
     @endif
 @endsection
