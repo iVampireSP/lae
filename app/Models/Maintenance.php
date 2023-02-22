28b6a9dd50e07e48f7815321438e98b31820779d
iVampireSP.com <im@ivampiresp.com>
2023-02-22 23:39:26 +0800

    使用 Cachable Trait

--- app/Models/Maintenance.php
+++ app/Models/Maintenance.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use GeneaLabs\LaravelModelCaching\Traits\Cachable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Maintenance extends Model
 {
+    use Cachable;
+
     public $timestamps = false;
 
     protected $fillable = [
