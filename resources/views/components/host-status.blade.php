7f7167a5d3c13301099cb218ece95f28a16fddde
iVampireSP.com <im@ivampiresp.com>
2023-02-28 18:05:13 +0800

    增加 更多状态

--- resources/views/components/host-status.blade.php
+++ resources/views/components/host-status.blade.php
@@ -4,6 +4,14 @@
             <span class="badge bg-success">运行中</span>
             @break
 
+        @case('active')
+            <span class="badge bg-success">激活</span>
+            @break
+
+        @case('expired')
+            <span class="badge bg-warning">到期</span>
+            @break
+
         @case('suspended')
             <span class="badge bg-warning">已暂停</span>
             @break
@@ -16,6 +24,10 @@
             <span class="badge bg-info">创建中</span>
             @break
 
+        @case('draft')
+            <span class="badge bg-secondary">草稿</span>
+            @break
+
         @case('error')
             <span class="badge bg-danger">错误</span>
             @break
@@ -26,6 +38,22 @@
 
         @case('locked')
             <span class="badge bg-danger">锁定</span>
+            @break
+
+        @case('one-time')
+            <span class="badge bg-danger">一次性</span>
+            @break
+
+        @case('recurring')
+            <span class="badge bg-success">循环</span>
+            @break
+
+        @case('free')
+            <span class="badge bg-info">免费</span>
+            @break
+
+        @case('trial')
+            <span class="badge bg-info">试用</span>
             @break
 
         @default
