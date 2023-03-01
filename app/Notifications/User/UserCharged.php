4ad741c18fd34d2f183e551c032c081c1454a8c2
iVampireSP.com <im@ivampiresp.com>
2023-03-01 22:55:06 +0800

    移除 未使用的构造函数

--- app/Notifications/User/UserCharged.php
+++ app/Notifications/User/UserCharged.php
@@ -13,16 +13,6 @@ class UserCharged extends Notification implements ShouldQueue
     use Queueable;
 
     /**
-     * Create a new notification instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
      * Get the notification's delivery channels.
      */
     public function via(): array
