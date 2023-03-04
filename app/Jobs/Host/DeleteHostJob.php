b4482da0f0dfb4b5c61b82ebc697748de64c91ed
iVampireSP.com <im@ivampiresp.com>
2023-03-04 18:33:07 +0800

    改进 主机错误删除任务

--- app/Jobs/Host/DeleteHostJob.php
+++ app/Jobs/Host/DeleteHostJob.php
@@ -36,6 +36,13 @@ class DeleteHostJob implements ShouldQueue
         // 查找不可用时间超过 3 天以上的 host
         (new Host)->where('status', 'unavailable')->where('unavailable_at', '<', now()->subDays(3))->delete();
 
+        // 查找错误状态超过 7 天以上的 host
+        (new Host)->where('status', 'error')->where('updated_at', '<', now()->subDays(7))->chunk(100, function ($hosts) {
+            foreach ($hosts as $host) {
+                dispatch(new HostJob($host, 'delete'));
+            }
+        });
+
         // 查找锁定时间超过 3 天以上的 host
         // (new Host)->where('status', 'locked')->where('locked_at', '<', now()->subDays(3))->chunk(100, function ($hosts) {
         //     foreach ($hosts as $host) {
